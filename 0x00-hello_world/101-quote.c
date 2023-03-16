0d60f8c62058b276e955c5fbd78a246a2b56239b
itsmbugua <samisaacmbugua@gmail.com>
2023-03-16 14:14:54 -0700

    quote c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A C that prints a simple line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
