cf08d1a61f22d9224de65f2058247dca8e526abb
itsmbugua <samisaacmbugua@gmail.com>
2023-03-16 05:11:45 -0700

    hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
- * main - writing lines using puts
+/**
+ * main - print a line using puts
  *
- * Return: 0
+ * return: 0
+ *
 */
+
 int main(void)
 {
-          puts("\"Programming is like building a multilingual puzzle");
-	  return(0);
-}	  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
