f9aab31f05795705dcf76c6865f76d48d85f41dd
itsmbugua <samisaacmbugua@gmail.com>
2023-03-17 06:35:37 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assign a random number to the variable n each time
+ * Return: (0)
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	x = n % 10;
+	printf("Last digit of %d is %d", n, x);
+	if (x > 5)
+	{
+		printf("and is greater than 5");
+	}
+	else if (x == 0)
+	{
+		printf("and is 0");
+	}
+	else(x < 6 && x != 0)
+	{
+		printf("and is less than 6 not 0");
+	}
+	print("\n")
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 26; s++)
+	{
+		putchar('a' + s);
+	}
+	putchar("\n")
+		return (0)
+}
