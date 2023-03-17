92307fae7d27be1c14cbe6d2b7e2c68e4894291e
itsmbugua <samisaacmbugua@gmail.com>
2023-03-17 03:16:39 -0700

    unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - A C that prints a simple line
  *
@@ -7,6 +8,6 @@
 int main(void)
 {
 
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
 	return (1);
 }
