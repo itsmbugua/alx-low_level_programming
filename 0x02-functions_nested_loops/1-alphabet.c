0a2129db48efd9d3de8c0c4626aca9ca35de0783
itsmbugua <samisaacmbugua@gmail.com>
2023-03-21 02:30:33 -0700

    printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - function that prints the alphabat
- * Return: always 0
+ *
  */
 
 void print_alphabet(void)
@@ -13,5 +13,5 @@ void print_alphabet(void)
 	_putchar(alph);
 
 	_putchar('\n');
-	return (0);
+
 }
