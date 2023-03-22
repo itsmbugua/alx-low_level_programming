63043c37da56e6ef7419a0ab5fa2c508ebf15c17
itsmbugua <samisaacmbugua@gmail.com>
2023-03-21 19:49:52 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: always 0
- */
-int main(void)
-{
-	int r;
 
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
